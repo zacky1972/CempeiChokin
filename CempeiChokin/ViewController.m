53ffcff72d19d719bf62811d39e0b5e7cbb16be2
中野 麻美 <nakano@vlan4-dhcp101.is.env.kitakyu-u.ac.jp>
2012-07-26 18:22:28 +0900

    ストーリーボードの修正．

--- CempeiChokin/ViewController.m
+++ CempeiChokin/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
