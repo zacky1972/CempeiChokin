c78318389f849db766943bee5a37e275b4db53d4
Takeshi-Y <t.yasunaga.0@gmail.com>
2012-07-28 00:02:46 +0900

    コメント文の追加とキャンセル，完了ボタンの動作を微妙に変更

--- CempeiChokin/GoalViewController.m
+++ CempeiChokin/GoalViewController.m
@@ -8,8 +8,15 @@
 
 #import "GoalViewController.h"
 
+<<<<<<< master
 @interface GoalViewController ()
 
+=======
+@interface GoalViewController (){
+    @private
+    NSString *tempValue; // 金額の保持に使う
+}
+>>>>>>> local
 @end
 
 @implementation GoalViewController
@@ -35,11 +42,6 @@
     [numberToolbar sizeToFit];
     ValueTextField.inputAccessoryView = numberToolbar;
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
@@ -59,6 +61,7 @@
 }
 
 - (IBAction)NameTextField_end:(id)sender {
+<<<<<<< master
     [ValueTextField becomeFirstResponder];  //ValueTextFieldに移動
 }
 
@@ -66,20 +69,69 @@
     [PeriodTextField becomeFirstResponder];  //PeriodTextFieldに移動したい
 }
 
+=======
+    // TODO: いつかここに値を保存する処理を書こう
+    [ValueTextField becomeFirstResponder];  //ValueTextFieldに移動
+}
+
+// 金額の設定
+- (IBAction)ValueTextField_begin:(id)sender {
+    // 既に値が入力されていた場合，表示されている値を数値に戻す (例)10,000円→10000
+    if([ValueTextField.text hasSuffix:@"円"]){
+        tempValue = ValueTextField.text;
+        NSString *tempValue2 = [tempValue substringToIndex:[tempValue length]-1]; // 円を消す(=語尾から一文字消す)
+        NSNumberFormatter *fmt = [[NSNumberFormatter alloc] init];
+        [fmt setPositiveFormat:@"#,##0"];
+        NSNumber *temp = [fmt numberFromString:tempValue2];            // ,を消す感じにする
+        ValueTextField.text = [NSString stringWithFormat:@"%@",temp];  // 消していく
+    }
+}
+
+- (IBAction)ValueTextField_end:(id)sender {
+    // TODO: いつかここに値を保存する処理を書こう
+    [PeriodTextField becomeFirstResponder];  //PeriodTextFieldに移動したい
+}
+
+// 期間の設定
+>>>>>>> local
 - (IBAction)PeriodTextField_end:(id)sender {
-    //ここでドラムを隠す
+    // TODO: ここでドラムを隠す
 }
 
 
 
 -(void)cancelNumberPad{
+<<<<<<< master
     [ValueTextField resignFirstResponder];
     ValueTextField.text = @"";
+=======
+    // 既に値が入っていた場合
+    if(tempValue != @"")
+        ValueTextField.text = tempValue;   // 元に戻す
+    // そうでもなかった場合
+    else
+        ValueTextField.text = @"";         // 値を消す
+    [ValueTextField resignFirstResponder]; // NumberPad消す(=テキストフィールドを選択していない状態にする)
+>>>>>>> local
 }
 
 -(void)doneWithNumberPad{
+<<<<<<< master
     //NSString *numberFromTheKeyboard = ValueTextField.text;
     [ValueTextField resignFirstResponder];
+=======
+    // 値が入っている場合
+    if([ValueTextField.text length] >= 1) {
+        NSNumber *value = [NSNumber numberWithInt:[ValueTextField.text intValue]]; // 文字を数値に変換
+        NSNumberFormatter *fmt = [[NSNumberFormatter alloc] init];                 // 形式変えるアレ
+        [fmt setPositiveFormat:@"#,##0"];                                          // 形式の指定
+        NSString *temp = [fmt stringForObjectValue:value];                         // アレ
+        ValueTextField.text = [NSString stringWithFormat:@"%@円",temp];            // 表示変える
+        // TODO: いつかここに値を保存する処理を書こう
+        // ???: ていうかここに移動するPeriodTextFieldに移動する処理書くんじゃね？
+    }
+    [ValueTextField resignFirstResponder];                                     // NumberPad消す
+>>>>>>> local
 }
 
 
