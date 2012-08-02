349f48e50ee0e6dcdd5785a66af3759554040b42
Takeshi-Y <t.yasunaga.0@gmail.com>
2012-08-02 16:01:08 +0900

    ほんとにサボってないよ

---
+++ CempeiChokin/Input.m
@@ -0,0 +1,13 @@
+//
+//  Input.m
+//  CempeiChokin
+//
+//  Created by Takeshi on 12/08/02.
+//  Copyright (c) 2012年 Nakano Asami. All rights reserved.
+//
+
+#import "Input.h"
+
+@implementation Input
+
+@end
