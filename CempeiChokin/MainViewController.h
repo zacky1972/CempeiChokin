daf93b0f9feb99222a8a5611aed523756711ee09
Eternal Forest <q9531065@eng.kitakyu-u.ac.jp>
2012-08-03 13:13:26 +0900

    ぐすたふまっくす！！

--- CempeiChokin/MainViewController.h
+++ CempeiChokin/MainViewController.h
@@ -15,7 +15,7 @@
 @interface MainViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
 {
     __weak IBOutlet UINavigationBar *MainNavigationBar;
-    IBOutlet UIScrollView *LogScroll;
+    __weak IBOutlet UIScrollView *LogScroll;
     __weak IBOutlet UILabel *BudgetLabel;
     __weak IBOutlet UILabel *ExpenseLabel;
     __weak IBOutlet UILabel *BalanceLabel;
