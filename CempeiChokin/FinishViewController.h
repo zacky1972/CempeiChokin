2b0729d4313ca45e9522279e778ca91116a27519
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-08 16:08:08 +0900

    なんか色々修正したよ

---
+++ CempeiChokin/FinishViewController.h
@@ -0,0 +1,18 @@
+//
+//  FinishViewController.h
+//  CempeiChokin
+//
+//  Created by CEMPEI on 12/07/25.
+//  Copyright (c) 2012年 CEMPEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Methods.h"
+#import "EditLog.h"
+
+@interface FinishViewController : UITableViewController{
+}
+- (IBAction)nextButton_down:(id)sender;
+- (IBAction)extendButton_down:(id)sender;
+
+@end
