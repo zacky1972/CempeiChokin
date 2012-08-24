47a3940e3254eea86e38afbdfc914cd9ec33ba99
A.Nakano <q9531045@eng.kitakyu-u.ac.jp>
2012-08-24 13:39:55 +0900

    用語画面用のWordsViewコントローラを用意した

---
+++ CempeiChokin/WordsViewController.h
@@ -0,0 +1,13 @@
+//
+//  WordsViewController.h
+//  CempeiChokin
+//
+//  Created by CEMPEI on 12/08/08.
+//  Copyright (c) 2012年 CEMPEI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WordsViewController : UITableViewController
+
+@end
